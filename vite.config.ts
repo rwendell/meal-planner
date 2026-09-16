ea90177656146614f6fcf5ebd2cb60696c8faa77
Ryan Wendell <ryanjwendell@gmail.com>
2026-09-15 19:05:03 -0500

    feat: add dev tools inspector (Alt+X) to dev

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
 		tailwindcss(),
 		sveltekit({
 			compilerOptions: {
+				inspector: true,
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
